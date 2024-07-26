85560ceabe89b7b86520b2de96a1460915b614c6
BillisC <26091373+BillisC@users.noreply.github.com>
2024-07-26 15:48:35 +0300

    multiple: Read description
    - Added a new defines header file
    - Added breakpoints and nop instructions when needed
    - Added and infinite while() loop in main() function
    - Added a variable for all include folder in CMakeLists
    
    - Moved _estack calculation in linker file
    - Fixed clock_init() getting stuck when calling SystemCoreClockUpdate()
    - Updated documentation of mcu_init related functions
    excuse the packed commit :p

--- src/drivers/mcu_init.c
+++ src/drivers/mcu_init.c
@@ -1,9 +1,17 @@
 #include "mcu_init.h"
 
+// Common
+#include "../common/defines.h"
+
+// Drivers
+#include "stm32f4xx.h"
+
+#include <stdint.h>
+
 static void clock_init(void) {
   /* Enable 8 MHz HSE oscillator (Source: STLINK) */
   RCC->CR |= RCC_CR_HSEBYP_Msk | RCC_CR_HSEON_Msk;
-  while (! (RCC->CR & RCC_CR_HSERDY_Msk));
+  while (! (RCC->CR & RCC_CR_HSERDY_Msk)) { ASM_NOP; };
 
   /* Enable power controller and change voltage
    * regulator scaling to 1. */
@@ -31,29 +39,22 @@ static void clock_init(void) {
 
   /* Enable PLL clock */
   RCC->CR |= RCC_CR_PLLON_Msk;
-  while (! (RCC->CR & RCC_CR_PLLRDY_Msk)) {
-    // WAIT
-  };
+  while (! (RCC->CR & RCC_CR_PLLRDY_Msk)) { ASM_NOP; };
 
   #if EN_OVERDRIVE == 1
   /* Enable overdrive mode */
   PWR->CR |= (PWR_CR_ODEN_Msk);
-  while (! (PWR->CSR & PWR_CSR_ODRDY)) {
-    // WAIT
-  };
+  while (! (PWR->CSR & PWR_CSR_ODRDY)) { ASM_NOP; };
+
   /* Switch internal voltage regulator to
    * overdrive mode. */
   PWR->CR |= (PWR_CR_ODSWEN_Msk);
-  while (! (PWR->CSR & PWR_CSR_ODSWRDY)) {
-    // WAIT
-  };
+  while (! (PWR->CSR & PWR_CSR_ODSWRDY)) { ASM_NOP; };
   #endif
 
   /* Select PLL as the system clock source */
   RCC->CFGR |= (RCC_CFGR_SW_PLL << RCC_CFGR_SW_Pos);
-  while (! (RCC->CFGR & RCC_CFGR_SWS_PLL)) {
-    // WAIT
-  };
+  while (! (RCC->CFGR & RCC_CFGR_SWS_PLL)) { ASM_NOP; };
 
   /* Inform CMSIS about the system clock */
   SystemCoreClockUpdate();
@@ -61,4 +62,7 @@ static void clock_init(void) {
 
 void mcu_init(void) {
   clock_init();
+  /* Set interrupt rate to 1 KHz (/180 MHZ) and enable interrupts */
+  SysTick_Config(180000);
+  __enable_irq();
 }
--- src/main.c
+++ src/main.c
@@ -1,8 +1,16 @@
+// Common
+#include "common/defines.h"
+
+// Drivers
+#include "stm32f4xx.h"
 #include "drivers/mcu_init.h"
 
 int main() {
   mcu_init();
+  ASM_BKPT;
+
   /* MAIN CODE GOES HERE */
+  while (TRUE) { ASM_NOP; }
 
   return 0;
 }

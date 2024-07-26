89da04453c0d1519d89b8fd11bbf515ebc8c535a
BillisC <26091373+BillisC@users.noreply.github.com>
2024-07-26 15:48:35 +0300

    multiple: Read description
    - Added a new defines header file
    - Added breakpoints and nop instructions when needed
    - Added and infinite while() loop in main() function
    - Added a variable for all include folder in CMakeLists
    
    - Moved _estack calculation in linker file
    - Fixed clock_init() getting stuck when calling SystemCoreClockUpdate()
    - Updated documentation of mcu_init related functions
    excuse the packed commit :p

---
+++ src/common/defines.h
@@ -0,0 +1,12 @@
+#ifndef DEFINES_H
+#define DEFINES_H
+
+// LOGIC
+#define TRUE 1
+#define FALSE 0
+
+// ASM INSTRUCTIONS
+#define ASM_BKPT __asm__("BKPT")
+#define ASM_NOP  __asm__("NOP")
+
+#endif
--- src/drivers/mcu_init.h
+++ src/drivers/mcu_init.h
@@ -5,20 +5,20 @@
 #ifndef MCU_INIT_H
 #define MCU_INIT_H
 
-#include "stm32f4xx.h"
-#include <stdint.h>
-
-// Clock Configuration
-/* 8MHz (HSE) / 4 (PLLM) = 2MHz
+/* Clock Configuration
+ * 8MHz (HSE) / 4 (PLLM) = 2MHz
  * 2MHz * 180 (PLLN) = 360MHz
  * 360MHz / 2 (PLLSRC) = 180MHz */
 #define PLLM_VAL 4
 #define PLLN_VAL 180
-#define PLLSRC_VAL 2
+#define PLLSRC_VAL 1
 
-// Power Regulation
+/* Power Regulation */
 #define EN_OVERDRIVE 1
 
+/**
+ * MCU initialization function.
+*/
 void mcu_init(void);
 
 #endif

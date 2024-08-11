b306a7b474974718328246ef61adce74c3cd6a46
BillisC <26091373+BillisC@users.noreply.github.com>
2024-08-11 12:30:28 +0300

    feat(dma_driver): Add DMA driver prototypes
    All prototypes will be located in the dma.h file.
    Note that the CI build will fail in the analysis step because of the empty dma.c file.

---
+++ src/drivers/dma.h
@@ -0,0 +1,201 @@
+/** @file dma.h
+ *  @brief Function prototypes for the DMA driver.
+ *
+ *  This file contains all of the enums, macros, and
+ *  function prototypes required for a functional DMA
+ *  driver.
+ *
+ *  DISCLAIMER: No burst or FIFO for the time being!
+ *
+ *  @author Vasileios Ch. (BillisC)
+ *  @bug None, yet.
+ */
+
+#ifndef DMA_H
+#define DMA_H
+
+/* -- Includes -- */
+#include <stdint.h>
+#include "stm32f4xx.h"
+
+/* -- Structs -- */
+struct __attribute__((packed)) DMAStreamRegs {
+  volatile uint32_t CR;
+  volatile uint32_t NDTR;
+  volatile uint32_t PAR;
+  volatile uint32_t M0AR;
+  volatile uint32_t M1AR;
+  volatile uint32_t FCR;
+};
+
+_Static_assert((sizeof(struct DMAStreamRegs)) == (sizeof(uint32_t) * 6U),
+               "DMA Sx register struct size mismatch. Is it aligned?");
+
+struct __attribute__((packed)) DMARegs {
+  volatile uint32_t LISR;
+  volatile uint32_t HISR;
+  volatile uint32_t LIFCR;
+  volatile uint32_t HIFCR;
+  volatile struct DMAStreamRegs S[8];
+};
+
+_Static_assert((sizeof(struct DMARegs)) ==
+                   (sizeof(uint32_t) * (4U + (6U * 8U))),
+               "DMA register struct size mismatch. Is it aligned?");
+
+#define DMA(x) (struct DMARegs *)(DMA1_BASE + ((x == 1) ? 0UL : 0x400UL))
+
+struct __attribute__((packed)) DMAStreamConfig {
+  volatile _Bool Circular     : 1;
+  volatile _Bool DoubleBuffer : 1;
+  volatile _Bool PerIncrement : 1;
+  volatile _Bool MemIncrement : 1;
+  volatile _Bool PerFlowCtrl  : 1;
+};
+
+_Static_assert((sizeof(struct DMAStreamConfig)) == (sizeof(uint8_t) * 1U),
+               "DMA Stream config struct size mismatch. Is it aligned?");
+
+struct __attribute__((packed)) DMAStreamISR {
+  volatile _Bool HTI  : 1;
+  volatile _Bool TCI  : 1;
+  volatile _Bool TEI  : 1;
+  volatile _Bool FEI  : 1;
+  volatile _Bool DMEI : 1;
+};
+
+_Static_assert((sizeof(struct DMAStreamISR)) == (sizeof(uint8_t) * 1U),
+               "DMA Stream Interrupt struct size mismatch. Is it aligned?");
+
+/* -- Enums -- */
+typedef enum dma_dir {
+  DMA_per2mem = 0x00,
+  DMA_mem2per = 0x01,
+  DMA_mem2mem = 0x02
+} dma_dir_t;
+
+typedef enum dma_datasize {
+  DMA_byte = 0x00,
+  DMA_hwrd = 0x01,
+  DMA_word = 0x02
+} dma_datasize_t;
+
+typedef enum dma_priority {
+  DMA_low = 0x00,
+  DMA_med = 0x01,
+  DMA_hig = 0x02,
+  DMA_vhi = 0x03
+} dma_priority_t;
+
+/**
+ * @brief Set the DMA source and destination addresses.
+ *
+ * Usually only the peripheral and M0 addresses are needed.
+ * In that case you shall set M1A to zero.
+ *
+ * @param dma The selected DMA
+ * @param stream The selected stream (0..7)
+ * @param PA The peripheral address
+ * @param M0A The memory 0 address
+ * @param M1A The memory 1 address
+ * @return None
+ */
+void dma_set_addresses(const uint8_t dma, const uint8_t stream,
+                       const uint32_t PA, const uint32_t M0A,
+                       const uint32_t M1A);
+
+/**
+ * @brief Set DMA transfers to specified configuration.
+ *
+ * The DMA datasizes are specified in the dma_datasize_t enum.
+ * Any other value will be ignored.
+ *
+ * @param dma The selected DMA (1..2)
+ * @param stream The selected stream (0..7)
+ * @param count The total number of data items
+ * @param msize The size of data stored in memory
+ * @param psize The size of data stored in the peripheral
+ * @return None
+ */
+void dma_configure_data(const uint8_t dma, const uint8_t stream,
+                        const uint16_t count, const dma_datasize_t msize,
+                        const dma_datasize_t psize);
+
+/**
+ * @brief Set DMA transfers to the desired direction.
+ *
+ * The DMA directions are specified in the dma_dir_t enum.
+ * Any other value will be ignored. After that set the
+ * addresses according to the chip's manual.
+ *
+ * @param dma The selected DMA (1..2)
+ * @param stream The selected stream (0..7)
+ * @param direction The transfer direction
+ * @return None
+ */
+void dma_set_direction(const uint8_t dma, const uint8_t stream,
+                       const dma_dir_t direction);
+
+/**
+ * @brief Set DMA stream to the specified configuration.
+ *
+ * The DMA stream options are specified in the DMAStreamConfig
+ * struct.
+ *
+ * @param dma The selected DMA (1..2)
+ * @param stream The selected stream (0..7)
+ * @param config The stream configuration
+ * @return None
+ */
+void dma_configure_stream(const uint8_t dma, const uint8_t stream,
+                          const struct DMAStreamConfig config);
+
+/**
+ * @brief Set the DMA stream channel and priority.
+ *
+ * The DMA priority levels are specified in the dma_priority_t
+ * enum. Any other value will be ignored. Refer to the chip's
+ * manual for the channel function matrix.
+ *
+ * @param dma The selected DMA (1..2)
+ * @param stream The selected stream (0..7)
+ * @param channel The selected channel (0..7)
+ * @param priority The stream priority level
+ * @return None
+ */
+void dma_set_channel(const uint8_t dma, const uint8_t stream,
+                     const uint8_t channel, const dma_priority_t priority);
+
+/**
+ * @brief Enabled the specified DMA stream interrupts.
+ *
+ * The available DMA interrupts are located in the DMAStreamISR
+ * struct.
+ *
+ * @param dma The selected DMA (1..2)
+ * @param stream The selected stream (0..7)
+ * @param config The interrupt configuration
+ * @return None
+ */
+void dma_set_interrupts(const uint8_t dma, const uint8_t stream,
+                        const struct DMAStreamISR config);
+
+/**
+ * @brief Enables DMA stream transfers.
+ *
+ * @param dma The selected DMA (1..2)
+ * @param stream The selected stream (0..7)
+ * @return None
+ */
+void dma_enable(const uint8_t dma, const uint8_t stream);
+
+/**
+ * @brief Disables DMA stream transfers.
+ *
+ * @param dma The selected DMA (1..2)
+ * @param stream The selected stream (0..7)
+ * @return None
+ */
+void dma_disable(const uint8_t dma, const uint8_t stream);
+
+#endif

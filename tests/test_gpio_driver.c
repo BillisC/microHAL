191a671092c48bb0c9f41fef72f43ff6872112ea
BillisC <26091373+BillisC@users.noreply.github.com>
2024-08-14 15:08:34 +0300

    feat(unit_testing): Completed GPIO driver unit tests
    These tests are purely for bitwise logic testing and may not reflect the actual behaviour of the hardware.
    Bank and pin tests fail as there are no checks in the driver functions.

--- tests/test_gpio_driver.c
+++ tests/test_gpio_driver.c
@@ -1,46 +1,210 @@
+/* -- Includes -- */
 #include <stdint.h>
 #include "unity.h"
 #include "gpio.h"
+#include "unity_internals.h"
 
 #define BANK_NUM(bank) ((uint8_t)bank - (uint8_t)'A')
 
-struct GPIORegs test_regs[8] = {0};
+/* 8 banks + 1 arbitrary */
+struct GPIORegs test_regs[9] = {0};
 struct GPIORegs *GPIO(const uint8_t bank) {
   return &test_regs[BANK_NUM(bank)];
 }
 
-void test_direction_functionality(void) {
-  uint8_t bank_to_test = 'A';
-
-  /* INPUT MODE */
-  gp_set_direction(bank_to_test, 5U, GP_DIR_IN);
-  TEST_ASSERT_EQUAL(0xA8000000UL, test_regs[BANK_NUM(bank_to_test)].MODER);
-  /* OUTPUT MODE */
-  gp_set_direction(bank_to_test, 0U, GP_DIR_OU);
-  TEST_ASSERT_EQUAL(0xA8000001UL, test_regs[BANK_NUM(bank_to_test)].MODER);
-  /* ALTERNATE MODE */
-  gp_set_direction(bank_to_test, 2U, GP_DIR_AL);
-  TEST_ASSERT_EQUAL(0xA8000021UL, test_regs[BANK_NUM(bank_to_test)].MODER);
-  /* ANALOG MODE */
-  gp_set_direction(bank_to_test, 6U, GP_DIR_AN);
-  TEST_ASSERT_EQUAL(0xA8003021UL, test_regs[BANK_NUM(bank_to_test)].MODER);
+void direction_highest_set_valid(void) {
+  test_regs[BANK_NUM('H')].MODER = 0x00000000UL;
+  gp_set_direction('H', 15U, GP_DIR_AN);
+  TEST_ASSERT_EQUAL_HEX32(0xC0000000UL, test_regs[BANK_NUM('H')].MODER);
 }
 
-void setUp() {
-  /* Initialize moder registers */
-  test_regs[0U].MODER = 0xA8000000UL;
-  test_regs[1U].MODER = 0x00000280UL;
-  /* Initialize ospeedr registers */
-  test_regs[1U].OSPEEDR = 0x000000C0UL;
-  /* Initialize pupdr registers */
-  test_regs[0U].PUPDR = 0x64000000UL;
-  test_regs[1U].PUPDR = 0x00000100UL;
+void direction_arbitrary_set_invalid_dir(void) {
+  test_regs[BANK_NUM('B')].MODER = 0x00000000UL;
+  gp_set_direction('B', 3U, 0x4U);
+  TEST_ASSERT_EQUAL_HEX32(0x00000000UL, test_regs[BANK_NUM('B')].MODER);
 }
+
+void direction_arbitrary_set_invalid_bank(void) {
+  test_regs[BANK_NUM('I')].MODER = 0x00000000UL;
+  gp_set_direction('I', 0U, GP_DIR_OU);
+  TEST_ASSERT_EQUAL_HEX32(0x00000000UL, test_regs[BANK_NUM('I')].MODER);
+}
+
+void direction_arbitrary_set_invalid_pin(void) {
+  test_regs[BANK_NUM('B')].MODER = 0x00000000UL;
+  gp_set_direction('B', 16U, GP_DIR_OU);
+  TEST_ASSERT_EQUAL_HEX32(0x00000000UL, test_regs[BANK_NUM('B')].MODER);
+}
+
+void otype_highest_set_valid(void) {
+  test_regs[BANK_NUM('H')].OTYPER = 0x00000000UL;
+  gp_set_output_type('H', 15U, GP_OTYPE_OD);
+  TEST_ASSERT_EQUAL_HEX32(0x00008000UL, test_regs[BANK_NUM('H')].OTYPER);
+}
+
+void otype_arbitrary_set_invalid_type(void) {
+  test_regs[BANK_NUM('A')].OTYPER = 0x00000000UL;
+  gp_set_output_type('A', 0U, 0x3U);
+  TEST_ASSERT_EQUAL_HEX32(0x00000000UL, test_regs[BANK_NUM('A')].OTYPER);
+}
+
+void otype_arbitrary_set_invalid_bank(void) {
+  test_regs[BANK_NUM('I')].OTYPER = 0x00000000UL;
+  gp_set_output_type('I', 0U, GP_OTYPE_OD);
+  TEST_ASSERT_EQUAL_HEX32(0x00000000UL, test_regs[BANK_NUM('I')].OTYPER);
+}
+
+void otype_arbitrary_set_invalid_pin(void) {
+  test_regs[BANK_NUM('A')].OTYPER = 0x00000000UL;
+  gp_set_output_type('A', 16U, GP_OTYPE_OD);
+  TEST_ASSERT_EQUAL_HEX32(0x00000000UL, test_regs[BANK_NUM('A')].OTYPER);
+}
+
+void speed_highest_set_valid(void) {
+  test_regs[BANK_NUM('H')].OSPEEDR = 0x00000000UL;
+  gp_set_speed('H', 15U, GP_SPEED_HIG);
+  TEST_ASSERT_EQUAL_HEX32(0xC0000000UL, test_regs[BANK_NUM('H')].OSPEEDR);
+}
+
+void speed_arbitrary_set_invalid_speed(void) {
+  test_regs[BANK_NUM('A')].OSPEEDR = 0x00000000UL;
+  gp_set_speed('A', 0U, 0x4U);
+  TEST_ASSERT_EQUAL_HEX32(0x00000000UL, test_regs[BANK_NUM('A')].OSPEEDR);
+}
+
+void speed_arbitrary_set_invalid_bank(void) {
+  test_regs[BANK_NUM('I')].OSPEEDR = 0x00000000UL;
+  gp_set_speed('I', 0U, GP_SPEED_HIG);
+  TEST_ASSERT_EQUAL_HEX32(0x00000000UL, test_regs[BANK_NUM('I')].OSPEEDR);
+}
+
+void speed_arbitrary_set_invalid_pin(void) {
+  test_regs[BANK_NUM('A')].OSPEEDR = 0x00000000UL;
+  gp_set_speed('A', 16U, GP_SPEED_HIG);
+  TEST_ASSERT_EQUAL_HEX32(0x00000000UL, test_regs[BANK_NUM('A')].OSPEEDR);
+}
+
+void pupd_highest_set_valid(void) {
+  test_regs[BANK_NUM('H')].PUPDR = 0x00000000UL;
+  gp_set_pupd('H', 15U, GP_PUPD_PLDO);
+  TEST_ASSERT_EQUAL_HEX32(0x80000000UL, test_regs[BANK_NUM('H')].PUPDR);
+}
+
+void pupd_arbitrary_set_invalid_pupd(void) {
+  test_regs[BANK_NUM('A')].PUPDR = 0x00000000UL;
+  gp_set_pupd('A', 0U, 0x3U);
+  TEST_ASSERT_EQUAL_HEX32(0x00000000UL, test_regs[BANK_NUM('A')].PUPDR);
+}
+
+void pupd_arbitrary_set_invalid_bank(void) {
+  test_regs[BANK_NUM('I')].PUPDR = 0x00000000UL;
+  gp_set_pupd('I', 0U, GP_PUPD_PLDO);
+  TEST_ASSERT_EQUAL_HEX32(0x00000000UL, test_regs[BANK_NUM('I')].PUPDR);
+}
+
+void pupd_arbitrary_set_invalid_pin(void) {
+  test_regs[BANK_NUM('A')].PUPDR = 0x00000000UL;
+  gp_set_pupd('A', 16U, GP_PUPD_PLDO);
+  TEST_ASSERT_EQUAL_HEX32(0x00000000UL, test_regs[BANK_NUM('A')].PUPDR);
+}
+
+void val_high_set_valid(void) {
+  test_regs[BANK_NUM('H')].BSSR = 0x00000000UL;
+  gp_set_val('H', 15U, 1U);
+  TEST_ASSERT_EQUAL_HEX32(0x00008000UL, test_regs[BANK_NUM('H')].BSSR);
+}
+
+void val_arbitrary_set_invalid_bank(void) {
+  test_regs[BANK_NUM('I')].BSSR = 0x00000000UL;
+  gp_set_val('I', 0U, 1U);
+  TEST_ASSERT_EQUAL_HEX32(0x00000000UL, test_regs[BANK_NUM('I')].BSSR);
+}
+
+void val_arbitrary_set_invalid_pin(void) {
+  test_regs[BANK_NUM('A')].BSSR = 0x00000000UL;
+  gp_set_val('A', 16U, 1U);
+  TEST_ASSERT_EQUAL_HEX32(0x00000000UL, test_regs[BANK_NUM('A')].BSSR);
+}
+
+void read_high_get_valid(void) {
+  test_regs[BANK_NUM('A')].IDR = 0x00000004UL;
+  TEST_ASSERT_EQUAL_HEX8(0x01U, gp_read_val('A', 2U));
+}
+
+void read_arbitrary_get_invalid_bank(void) {
+  test_regs[BANK_NUM('I')].IDR = 0x00000004UL;
+  TEST_ASSERT_EQUAL_HEX8(0x00U, gp_read_val('I', 2U));
+}
+
+void read_arbitrary_get_invalid_pin(void) {
+  test_regs[BANK_NUM('A')].IDR = 0x00010000UL;
+  TEST_ASSERT_EQUAL_HEX8(0x00U, gp_read_val('A', 16U));
+}
+
+void af_highest_set_valid(void) {
+  test_regs[BANK_NUM('H')].AFR[1] = 0x00000000UL;
+  gp_set_af('H', 15U, 15U);
+  TEST_ASSERT_EQUAL_HEX32(0xF0000000UL, test_regs[BANK_NUM('H')].AFR[1]);
+}
+
+void af_arbitrary_set_invalid_af(void) {
+  test_regs[BANK_NUM('A')].AFR[0] = 0x00000000UL;
+  gp_set_af('A', 0U, 16U);
+  TEST_ASSERT_EQUAL_HEX32(0x00000000UL, test_regs[BANK_NUM('A')].AFR[0]);
+}
+
+void af_arbitrary_set_invalid_bank(void) {
+  test_regs[BANK_NUM('I')].AFR[0] = 0x00000000UL;
+  gp_set_af('I', 0U, 1U);
+  TEST_ASSERT_EQUAL_HEX32(0x00000000UL, test_regs[BANK_NUM('I')].AFR[0]);
+}
+
+void af_arbitrary_set_invalid_pin(void) {
+  test_regs[BANK_NUM('A')].AFR[1] = 0x00000000UL;
+  gp_set_af('A', 16U, 1U);
+  TEST_ASSERT_EQUAL_HEX32(0x00000000UL, test_regs[BANK_NUM('A')].AFR[1]);
+}
+
+void setUp() {}
 
 void tearDown() {}
 
 int main(void) {
   UNITY_BEGIN();
-  RUN_TEST(test_direction_functionality);
+
+  /* gp_set_direction() */
+  RUN_TEST(direction_highest_set_valid);
+  RUN_TEST(direction_arbitrary_set_invalid_dir);
+  RUN_TEST(direction_arbitrary_set_invalid_bank);
+  RUN_TEST(direction_arbitrary_set_invalid_pin);
+  /* gp_set_output_type() */
+  RUN_TEST(otype_highest_set_valid);
+  RUN_TEST(otype_arbitrary_set_invalid_type);
+  RUN_TEST(otype_arbitrary_set_invalid_bank);
+  RUN_TEST(otype_arbitrary_set_invalid_pin);
+  /* gp_set_speed() */
+  RUN_TEST(speed_highest_set_valid);
+  RUN_TEST(speed_arbitrary_set_invalid_speed);
+  RUN_TEST(speed_arbitrary_set_invalid_bank);
+  RUN_TEST(speed_arbitrary_set_invalid_pin);
+  /* gp_set_pupd() */
+  RUN_TEST(pupd_highest_set_valid);
+  RUN_TEST(pupd_arbitrary_set_invalid_pupd);
+  RUN_TEST(pupd_arbitrary_set_invalid_bank);
+  RUN_TEST(pupd_arbitrary_set_invalid_pin);
+  /* gp_set_val() */
+  RUN_TEST(val_high_set_valid);
+  RUN_TEST(val_arbitrary_set_invalid_bank);
+  RUN_TEST(val_arbitrary_set_invalid_pin);
+  /* gp_read_val() */
+  RUN_TEST(read_high_get_valid);
+  RUN_TEST(read_arbitrary_get_invalid_bank);
+  RUN_TEST(read_arbitrary_get_invalid_pin);
+  /* gp_set_af() */
+  RUN_TEST(af_highest_set_valid);
+  RUN_TEST(af_arbitrary_set_invalid_af);
+  RUN_TEST(af_arbitrary_set_invalid_bank);
+  RUN_TEST(af_arbitrary_set_invalid_pin);
+
   return UNITY_END();
 }

21a260d0c651e5f3f28d4114bb407da6dbcc1180
BillisC <26091373+BillisC@users.noreply.github.com>
2024-08-18 11:41:16 +0300

    fix!(mcu_init): The clock was configured incorrectly
    Somehow I used PLLSRC as PLLP and this resulted in a /4 division of 360MHz.
    It should now truly operate at 180MHz. Also this should be the cause of the DMA USART2
    bug.

--- src/drivers/mcu_init.h
+++ src/drivers/mcu_init.h
@@ -35,16 +35,17 @@
 /** Clock Configuration
  * 8MHz (HSE) / 4 (PLLM) = 2MHz
  * 2MHz * 180 (PLLN) = 360MHz
- * 360MHz / 2 (PLLSRC) = 180MHz
- * -- WIP --
+ * 360MHz / 2 (PLLP) = 180MHz
  * 180MHz / 4 (PPRE1) = 45MHz (APB1)
  * 180MHz / 2 (PPRE2) = 90MHz (APB2)
  */
-#define PLLM_VAL   4U
-#define PLLN_VAL   180U
-#define PLLSRC_VAL 1U // == 2
-#define PPRE1_VAL  5U // == 4
-#define PPRE2_VAL  4U // == 2
+#define PLLM_VAL  4U
+#define PLLN_VAL  180U
+#define PLLP_VAL  0U // == 2
+#define PPRE1_VAL 5U // == 4
+#define PPRE2_VAL 4U // == 2
+
+#define PLLSRC_VAL 1U // Use HSE clock
 
 _Static_assert((((8 / 4) * 180) / 2) == (SYS_CLK),
                "System clock calculation mismatch. Did you update SYS_CLK?");

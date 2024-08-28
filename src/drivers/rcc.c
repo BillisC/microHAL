f95f73ff47e79e9a0fd0d44fb7be589d51a135c4
BillisC <26091373+BillisC@users.noreply.github.com>
2024-08-28 17:12:37 +0300

    feat(rcc_driver): Initial RCC module implementation
    At its current state it can power peripherals, oscillators, and configure PLL.
    There was an idea about masking the ahb/apb register bits to avoid reserved
    writes but the macros went brrrr.

---
+++ src/drivers/rcc.c
@@ -0,0 +1,197 @@
+/** @file rcc.c
+ *  @brief Function defines for the RCC driver.
+ *
+ *  This file contains all of the function definitions
+ *  declared in rcc.h.
+ *
+ *  @author Vasileios Ch. (BillisC)
+ *  @bug None, yet.
+ */
+
+/* Includes */
+#include "defines.h"
+#include "rcc.h"
+
+void rcc_configure_ahb_prescaler(const rcc_ahb_prescaler_t value) {
+  /* Make sure the prescaler value is valid */
+  switch (value) {
+    case RCC_AHB_PRESCALER_DIV1:
+    case RCC_AHB_PRESCALER_DIV2:
+    case RCC_AHB_PRESCALER_DIV4:
+    case RCC_AHB_PRESCALER_DIV8:
+    case RCC_AHB_PRESCALER_DIV16:
+    case RCC_AHB_PRESCALER_DIV64:
+    case RCC_AHB_PRESCALER_DIV128:
+    case RCC_AHB_PRESCALER_DIV256:
+    case RCC_AHB_PRESCALER_DIV512: break;
+    default: return;
+  }
+
+  /* Change the AHB prescaler */
+  struct RCCRegs *regs = RCC_;
+  REG32 cfgr = regs->CFGR;
+  cfgr &= ~(RCC_CFGR_HPRE_Msk); // clear first
+  cfgr |= (value << RCC_CFGR_HPRE_Pos);
+
+  regs->CFGR = cfgr;
+  ASM_DSB;
+}
+
+void rcc_configure_apb_prescaler(const uint8_t apb,
+                                 const rcc_apb_prescaler_t value) {
+  /* Make sure the prescaler value is valid */
+  switch (value) {
+    case RCC_APB_PRESCALER_DIV1:
+    case RCC_APB_PRESCALER_DIV2:
+    case RCC_APB_PRESCALER_DIV4:
+    case RCC_APB_PRESCALER_DIV8:
+    case RCC_APB_PRESCALER_DIV16: break;
+    default: return;
+  }
+
+  if (!(apb != 1 && apb != 2)) {
+    return;
+  } else {
+    /* Change the APB prescaler */
+    struct RCCRegs *regs = RCC_;
+    REG32 cfgr = regs->CFGR;
+    cfgr &= ~(RCC_CFGR_HPRE_Msk); // clear first
+    cfgr |= (value << RCC_CFGR_HPRE_Pos);
+
+    regs->CFGR = cfgr;
+    ASM_DSB;
+  }
+}
+
+void rcc_configure_pll_clk(const struct RCCPLLConfig config) {
+  /* Check if the configuration is valid, and if it's not,
+   * ignore the bad values. This is done because the user
+   * may want to leave some values untouched (by leaving
+   * them zeroed). */
+  const uint32_t good_pllr = (config.PLLR >= 2) ? RCC_PLLCFGR_PLLR_Msk : 0U;
+  const uint32_t good_pllq = (config.PLLQ >= 2) ? RCC_PLLCFGR_PLLQ_Msk : 0U;
+  const uint32_t good_plln = (config.PLLN >= 50) ? RCC_PLLCFGR_PLLN_Msk : 0U;
+  const uint32_t good_pllm = (config.PLLM >= 2) ? RCC_PLLCFGR_PLLM_Msk : 0U;
+
+  /* Configure the PLL clock */
+  struct RCCRegs *regs = RCC_;
+  REG32 pllcfgr = regs->PLLCFGR;
+  pllcfgr &= ~(good_pllr | good_pllq | good_plln | good_pllm |
+               RCC_PLLCFGR_PLLSRC_Msk); // clear first
+  pllcfgr |= (((config.PLLR << RCC_PLLCFGR_PLLR_Pos) & good_pllr) |
+              ((config.PLLQ << RCC_PLLCFGR_PLLQ_Pos) & good_pllq) |
+              ((config.PLLN << RCC_PLLCFGR_PLLN_Pos) & good_plln) |
+              ((config.PLLM << RCC_PLLCFGR_PLLM_Pos) & good_pllm) |
+              (config.UseHSE << RCC_PLLCFGR_PLLSRC_Pos));
+
+  regs->PLLCFGR = pllcfgr;
+  ASM_DSB;
+}
+
+void rcc_enable_osc(const rcc_osc_t osc) {
+  struct RCCRegs *regs = RCC_;
+  switch (osc) {
+    case RCC_OSC_HSI:
+      regs->CR |= RCC_CR_HSION_Msk;
+      while (!(regs->CR & RCC_CR_HSIRDY_Msk)) { ASM_NOP; };
+      break;
+    case RCC_OSC_HSE:
+      regs->CR |= (RCC_CR_HSEBYP_Msk | RCC_CR_HSEON_Msk);
+      while (!(regs->CR & RCC_CR_HSERDY_Msk)) { ASM_NOP; };
+      break;
+    case RCC_OSC_LSI:
+      regs->CSR |= RCC_CSR_LSION_Msk;
+      while (!(regs->CSR & RCC_CSR_LSIRDY_Msk)) { ASM_NOP; };
+      break;
+    case RCC_OSC_LSE:
+      regs->BDCR |= RCC_BDCR_LSEON_Msk;
+      while (!(regs->BDCR & RCC_BDCR_LSERDY_Msk)) { ASM_NOP; };
+      break;
+    case RCC_OSC_PLL:
+      regs->CR |= RCC_CR_PLLON_Msk;
+      while (!(regs->CR & RCC_CR_PLLRDY_Msk)) { ASM_NOP; }
+      break;
+    case RCC_OSC_PLLSAI:
+      regs->CR |= RCC_CR_PLLSAION_Msk;
+      while (!(regs->CR & RCC_CR_PLLSAIRDY_Msk)) { ASM_NOP; };
+      break;
+    case RCC_OSC_PLLI2S:
+      regs->CR |= RCC_CR_PLLI2SON_Msk;
+      while (!(regs->CR & RCC_CR_PLLI2SRDY_Msk)) { ASM_NOP; }
+      break;
+    default: return;
+  }
+}
+
+void rcc_disable_osc(const rcc_osc_t osc) {
+  struct RCCRegs *regs = RCC_;
+  switch (osc) {
+    case RCC_OSC_HSI: regs->CR &= ~(RCC_CR_HSION_Msk); break;
+    case RCC_OSC_HSE: regs->CR &= ~(RCC_CR_HSEON_Msk); break;
+    case RCC_OSC_LSI: regs->CSR &= ~(RCC_CSR_LSION_Msk); break;
+    case RCC_OSC_LSE: regs->BDCR &= ~(RCC_BDCR_LSEON_Msk); break;
+    case RCC_OSC_PLL: regs->CR &= ~(RCC_CR_PLLON_Msk); break;
+    case RCC_OSC_PLLSAI: regs->CR &= ~(RCC_CR_PLLSAION_Msk); break;
+    case RCC_OSC_PLLI2S: regs->CR &= ~(RCC_CR_PLLI2SON_Msk); break;
+    default: return;
+  }
+  ASM_DSB;
+}
+
+void rcc_set_systemclock_src(const rcc_systemclock_src_t source) {
+  /* Check that the source is correct */
+  switch (source) {
+    case RCC_SYSTEMCLOCK_SRC_HSI:
+    case RCC_SYSTEMCLOCK_SRC_HSE:
+    case RCC_SYSTEMCLOCK_SRC_PLL:
+    case RCC_SYSTEMCLOCK_SRC_PLR: break;
+    default: return;
+  }
+
+  /* Set system clock source */
+  struct RCCRegs *regs = RCC_;
+  REG32 cfgr = regs->CFGR;
+  cfgr &= ~(RCC_CFGR_SW_Msk);
+  cfgr |= (source << RCC_CFGR_SW_Pos);
+
+  regs->CFGR = cfgr;
+  ASM_DSB;
+}
+
+void rcc_enable_peripheral_clk(const rcc_clk_periph_t peripheral) {
+  /* Set peripheral clock bit */
+  struct RCCRegs *regs = RCC_;
+  if (peripheral < BIT(5)) {
+    regs->AHB1ENR |= BIT(peripheral);
+  } else if (peripheral < BIT(6)) {
+    regs->AHB2ENR |= BIT(peripheral >> 5);
+  } else if (peripheral < BIT(7)) {
+    regs->AHB3ENR |= BIT(peripheral >> 6);
+  } else if (peripheral < BIT(8)) {
+    regs->APB1ENR |= BIT(peripheral >> 7);
+  } else if (peripheral < BIT(9)) {
+    regs->APB2ENR |= BIT(peripheral >> 8);
+  } else {
+    return;
+  }
+  ASM_DSB;
+}
+
+void rcc_disable_peripheral_clk(const rcc_clk_periph_t peripheral) {
+  /* Clear peripheral clock bit */
+  struct RCCRegs *regs = RCC_;
+  if (peripheral < BIT(5)) {
+    regs->AHB1ENR &= ~BIT(peripheral);
+  } else if (peripheral < BIT(6)) {
+    regs->AHB2ENR &= ~BIT(peripheral >> 5);
+  } else if (peripheral < BIT(7)) {
+    regs->AHB3ENR &= ~BIT(peripheral >> 6);
+  } else if (peripheral < BIT(8)) {
+    regs->APB1ENR &= ~BIT(peripheral >> 7);
+  } else if (peripheral < BIT(9)) {
+    regs->APB2ENR &= ~BIT(peripheral >> 8);
+  } else {
+    return;
+  }
+  ASM_DSB;
+}
---
+++ src/drivers/rcc.h
@@ -0,0 +1,336 @@
+/** @file rcc.h
+ *  @brief Function prototypes for the RCC driver.
+ *
+ *  This file contains all of the enums, macros, and
+ *  function prototypes required for a functional RCC
+ *  driver.
+ *
+ *  @author Vasileios Ch. (BillisC)
+ *  @bug None, yet.
+ */
+
+#ifndef RCC_H
+#define RCC_H
+
+/* -- Includes -- */
+#include <stdint.h>
+#include "stm32f4xx.h"
+#include "defines.h"
+
+/* -- Structs -- */
+/**
+ *  @brief Contains RCC registers
+ */
+struct __attribute__((packed)) RCCRegs {
+  REG32 CR;
+  REG32 PLLCFGR;
+  REG32 CFGR;
+  REG32 CIR;
+  REG32 AHBxRSTR[3];
+  REG32 _reserved1;
+  REG32 APB1RSTR;
+  REG32 APB2RSTR;
+  REG32 _reserved2[2];
+  REG32 AHB1ENR;
+  REG32 AHB2ENR;
+  REG32 AHB3ENR;
+  REG32 _reserved3;
+  REG32 APB1ENR;
+  REG32 APB2ENR;
+  REG32 _reserved4[2];
+  REG32 AHB1LPENR;
+  REG32 AHB2LPENR;
+  REG32 AHB3LPENR;
+  REG32 _reserved5;
+  REG32 APB1LPENR;
+  REG32 APB2LPENR;
+  REG32 _reserved6[2];
+  REG32 BDCR;
+  REG32 CSR;
+  REG32 _reserved7[2];
+  REG32 SSCGR;
+  REG32 PLLI2SCFGR;
+  REG32 PLLSAICFGR;
+  REG32 DCKCFGR;
+  REG32 CKGATENR;
+  REG32 DCKCFGR2;
+};
+
+_Static_assert((sizeof(struct RCCRegs)) == (sizeof(uint32_t) * 38U),
+               "RCC register struct size mismatch. Is it aligned?");
+
+#ifndef UTESTS
+#define RCC_ (struct RCCRegs *)RCC_BASE
+#else
+extern struct RCCRegs *RCC_;
+#endif
+
+/**
+ *  @brief Contains PLL configuration values
+ */
+struct __attribute__((packed)) RCCPLLConfig {
+  uint8_t PLLM  : 6;
+  uint8_t PLLP  : 2;
+  uint8_t PLLQ  : 4;
+  uint8_t PLLR  : 3;
+  uint16_t PLLN : 9;
+  _Bool UseHSE  : 1;
+};
+
+_Static_assert((sizeof(struct RCCPLLConfig)) == (sizeof(uint8_t) * 4U),
+               "RCC PLL configuration struct size mismatch. Is it aligned?");
+
+/* -- Enums -- */
+/**
+ *  @brief Contains RCC oscillators
+ */
+typedef enum rcc_osc {
+  RCC_OSC_HSI = 0x0,
+  RCC_OSC_HSE = 0x1,
+  RCC_OSC_LSI = 0x2,
+  RCC_OSC_LSE = 0x3,
+  RCC_OSC_PLL = 0x4,
+  RCC_OSC_PLLSAI = 0x5,
+  RCC_OSC_PLLI2S = 0x6,
+} rcc_osc_t;
+
+/**
+ *  @brief Contains AHB/APB peripheral clocks
+ */
+typedef enum rcc_clk_periph {
+  /* ---------- AHB1 ---------- */
+  RCC_CLK_GPIOA = 0U,
+  RCC_CLK_GPIOB,
+  RCC_CLK_GPIOD,
+  RCC_CLK_GPIOC,
+  RCC_CLK_GPIOE,
+  RCC_CLK_GPIOF,
+  RCC_CLK_GPIOG,
+  RCC_CLK_GPIOH,
+  RCC_CLK_GPIOI,
+  RCC_CLK_GPIOJ,
+  RCC_CLK_GPIOK,
+  RCC_CLK_CRC = 12U,
+  RCC_CLK_BKPSRAM = 18U,
+  RCC_CLK_CCMDATARAM = 20U,
+  RCC_CLK_DMA1,
+  RCC_CLK_DMA2,
+  RCC_CLK_DMA2D,
+  RCC_CLK_ETHMAC = 25U,
+  RCC_CLK_ETHMACTX,
+  RCC_CLK_ETHMACRX,
+  RCC_CLK_ETHMACPTP,
+  RCC_CLK_OTGHS,
+  RCC_CLK_OTGHSULPI,
+
+  /* ---------- AHB2 ---------- */
+  RCC_CLK_DCMI = BIT(5),
+  RCC_CLK_CRYP = BIT(5) + 4U,
+  RCC_CLK_HASH,
+  RCC_CLK_RNG,
+  RCC_CLK_OTGFS,
+
+  /* ---------- AHB3 ---------- */
+  RCC_CLK_FMC = BIT(6),
+  RCC_CLK_QSPI,
+
+  /* ---------- APB1 ---------- */
+  RCC_CLK_TIM2 = BIT(7),
+  RCC_CLK_TIM3,
+  RCC_CLK_TIM4,
+  RCC_CLK_TIM5,
+  RCC_CLK_TIM6,
+  RCC_CLK_TIM7,
+  RCC_CLK_TIM12,
+  RCC_CLK_TIM13,
+  RCC_CLK_TIM14,
+  RCC_CLK_WWDG = BIT(7) + 11U,
+  RCC_CLK_SPI2 = BIT(7) + 14U,
+  RCC_CLK_SPI3,
+  RCC_CLK_SPDIFRX,
+  RCC_CLK_USART2,
+  RCC_CLK_USART3,
+  RCC_CLK_UART4,
+  RCC_CLK_UART5,
+  RCC_CLK_I2C1,
+  RCC_CLK_I2C2,
+  RCC_CLK_I2C3,
+  RCC_CLK_FMPI2C1,
+  RCC_CLK_CAN1,
+  RCC_CLK_CAN2,
+  RCC_CLK_CEC,
+  RCC_CLK_PWR,
+  RCC_CLK_DAC,
+  RCC_CLK_UART7,
+  RCC_CLK_UART8,
+
+  /* ---------- APB2 ---------- */
+  RCC_CLK_TIM1 = BIT(8),
+  RCC_CLK_TIM8,
+  RCC_CLK_USART1 = BIT(8) + 4U,
+  RCC_CLK_USART6,
+  RCC_CLK_ADC1 = BIT(8) + 8U,
+  RCC_CLK_ADC2,
+  RCC_CLK_ADC3,
+  RCC_CLK_SDIO,
+  RCC_CLK_SPI1,
+  RCC_CLK_SPI4,
+  RCC_CLK_SYSCFG,
+  RCC_CLK_TIM9 = BIT(8) + 16U,
+  RCC_CLK_TIM10,
+  RCC_CLK_TIM11,
+  RCC_CLK_SPI5 = BIT(8) + 20U,
+  RCC_CLK_SPI6,
+  RCC_CLK_SAI1,
+  RCC_CLK_SAI2,
+  RCC_CLK_LTDC = BIT(8) + 26U,
+  RCC_CLK_DSI
+} rcc_clk_periph_t;
+
+/**
+ *  @brief Contains RCC APBx prescaler dividers
+ */
+typedef enum rcc_apb_prescaler {
+  RCC_APB_PRESCALER_DIV1 = 0x0,
+  RCC_APB_PRESCALER_DIV2 = 0x4,
+  RCC_APB_PRESCALER_DIV4 = 0x5,
+  RCC_APB_PRESCALER_DIV8 = 0x6,
+  RCC_APB_PRESCALER_DIV16 = 0x7,
+} rcc_apb_prescaler_t;
+
+/**
+ *  @brief Contains RCC AHB prescaler dividers
+ */
+typedef enum rcc_ahb_prescaler {
+  RCC_AHB_PRESCALER_DIV1 = 0x0,
+  RCC_AHB_PRESCALER_DIV2 = 0x8,
+  RCC_AHB_PRESCALER_DIV4 = 0x9,
+  RCC_AHB_PRESCALER_DIV8 = 0xA,
+  RCC_AHB_PRESCALER_DIV16 = 0xB,
+  RCC_AHB_PRESCALER_DIV64 = 0xC,
+  RCC_AHB_PRESCALER_DIV128 = 0xD,
+  RCC_AHB_PRESCALER_DIV256 = 0xE,
+  RCC_AHB_PRESCALER_DIV512 = 0xF,
+} rcc_ahb_prescaler_t;
+
+/**
+ *  @brief Contains RCC system clock sources
+ */
+typedef enum rcc_systemclock_src {
+  RCC_SYSTEMCLOCK_SRC_HSI = 0x0,
+  RCC_SYSTEMCLOCK_SRC_HSE = 0x1,
+  RCC_SYSTEMCLOCK_SRC_PLL = 0x2, /** PLL_P */
+  RCC_SYSTEMCLOCK_SRC_PLR = 0x3, /** PLL_R */
+} rcc_systemclock_src_t;
+
+/**
+ *  @brief Set AHB prescaler to the desired value
+ *
+ *  The available prescale dividers are available in
+ *  the rcc_ahb_prescaler_t enum. Any other value will
+ *  be ignored. A DSB instruction is inserted after each
+ *  RCC register write as per errata.
+ *
+ *  @param value The prescaler divide value
+ *  @return None
+ */
+void rcc_configure_ahb_prescaler(const rcc_ahb_prescaler_t value);
+
+/**
+ *  @brief Set APBx prescaler to the desired value
+ *
+ *  The available prescale dividers are available in
+ *  the rcc_apb_prescaler_t enum. Any other value will
+ *  be ignored. A DSB instruction is inserted after each
+ *  RCC register write as per errata.
+ *
+ *  @param apb The selected APB (1..2)
+ *  @param value The prescaler divide value
+ *  @return None
+ */
+void rcc_configure_apb_prescaler(const uint8_t apb,
+                                 const rcc_apb_prescaler_t value);
+
+/**
+ *  @brief Sets the PLL clocks according to config
+ *
+ *  The configuration values can be found in the
+ *  RCCPLLConfig struct. Extra care must be taken with
+ *  PLL values as their bits are not the same and
+ *  wrapping may happen. A DSB instruction is inserted
+ *  after each RCC register write as per errata.
+ *
+ *  @param peripheral The peripheral clock
+ *  @return None
+ */
+void rcc_configure_pll_clk(const struct RCCPLLConfig config);
+
+/**
+ *  @brief Enable the specified oscillator
+ *
+ *  The available oscilattors are available in the
+ *  rcc_osc_t enum. Any other value will be ignored.
+ *  A DSB instruction is inserted after each RCC
+ *  register write as per errata.
+ *
+ *  @param osc The selected oscillator
+ *  @return None
+ */
+void rcc_enable_osc(const rcc_osc_t osc);
+
+/**
+ *  @brief Disable the specified oscillator
+ *
+ *  The available oscilattors are available in the
+ *  rcc_osc_t enum. Any other value will be ignored.
+ *  A DSB instruction is inserted after each RCC
+ *  register write as per errata.
+ *
+ *  @param osc The selected oscillator
+ *  @return None
+ */
+void rcc_disable_osc(const rcc_osc_t osc);
+
+/**
+ *  @brief Sets the system clock to the specified source
+ *
+ *  The available systemclock sources are located in
+ *  the rcc_systemclock_src_t enum. Any other value
+ *  will be ignored. A DSB instruction is inserted after
+ *  each RCC register write as per errata.
+ *
+ *  @param source The selected clock source
+ *  @return None
+ */
+void rcc_set_systemclock_src(const rcc_systemclock_src_t source);
+
+/**
+ *  @brief Enables the specified peripheral clock
+ *
+ *  The available peripherals can be found in the
+ *  rcc_clk_periph_t enum. Any other value will be
+ *  ignored. A DSB instruction is inserted after each
+ *  RCC register write as per errata.
+ *
+ *  DISCLAIMER: Only enable peripherals that actually
+ *  exist on hardware!
+ *
+ *  @param peripheral The peripheral clock
+ *  @return None
+ */
+void rcc_enable_peripheral_clk(const rcc_clk_periph_t peripheral);
+
+/**
+ *  @brief Disables the specified peripheral clock
+ *
+ *  The available peripherals can be found in the
+ *  rcc_clk_periph_t enum. Any other value will be
+ *  ignored. A DSB instruction is inserted after each
+ *  RCC register write as per errata.
+ *
+ *  @param peripheral The peripheral clock
+ *  @return None
+ */
+void rcc_disable_peripheral_clk(const rcc_clk_periph_t peripheral);
+
+#endif
